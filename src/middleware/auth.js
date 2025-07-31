b57aa496a2cf0b40b1bf269d2b0c8e684e1b8762
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-31 23:39:30 +0700

    Add files via upload

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,1 +1,21 @@
+const { DASH_PASS } = require('../config');
 
+function requireAuth(req, res, next) {
+  const openPaths = [
+    "/status", "/login", "/login-submit", "/track", "/check", "/complete", 
+    "/bond", "/join", "/send-job", "/start-job", "/status/", "/graph", 
+    "/disconnected", "/jadwal", "/schedule", "/current-subject", "/order"
+  ];
+  
+  if (openPaths.some(p => req.path.startsWith(p))) {
+    return next();
+  }
+  
+  if (req.cookies?.dash_auth === DASH_PASS) {
+    return next();
+  }
+
+  return res.redirect("/login");
+}
+
+module.exports = requireAuth;
